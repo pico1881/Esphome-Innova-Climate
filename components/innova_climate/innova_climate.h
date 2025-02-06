33f2139eaccd7298e6c45b2093a9f9df776b8516
pico1881 <82042085+pico1881@users.noreply.github.com>
2025-02-06 15:50:27 +0100

    Update innova_climate.h

--- components/innova_climate/innova_climate.h
+++ components/innova_climate/innova_climate.h
@@ -58,10 +58,6 @@ class Innova : public esphome::climate::Climate, public PollingComponent, public
   void writeModbusRegister(WriteableData write_data);
 
   void control(const climate::ClimateCall &call) override; 
-
- // void handle_mode(const climate::ClimateCall &call);
- // void handle_fan_mode(const climate::ClimateCall &call);
- // void handle_target_temperature(const climate::ClimateCall &call);
 };
 
 }  // namespace innova
