e0ea2b3428065b9f340a892b8aab8d8816cd1a76
pico1881 <82042085+pico1881@users.noreply.github.com>
2025-02-07 14:28:37 +0100

    Update climate.py

--- components/innova_climate/climate.py
+++ components/innova_climate/climate.py
@@ -1,12 +1,12 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import climate, modbus
+from esphome.components import climate, modbus, sensor
 
 from esphome.const import (
     CONF_ID, 
 )
 
-AUTO_LOAD = ['modbus']
+AUTO_LOAD = ['modbus', 'sensor']
 
 innova_ns = cg.esphome_ns.namespace("innova")
 
@@ -18,6 +18,31 @@ CONFIG_SCHEMA = (
     climate.CLIMATE_SCHEMA.extend(
         {
             cv.GenerateID(): cv.declare_id(Innova),
+            cv.Optional(CONF_AIR_TEMPERATURE): sensor.sensor_schema(
+                unit_of_measurement=UNIT_CELSIUS,
+                accuracy_decimals=1,
+                device_class=DEVICE_CLASS_TEMPERATURE,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_WATER_TEMPERATURE): sensor.sensor_schema(
+                unit_of_measurement=UNIT_CELSIUS,
+                accuracy_decimals=1,
+                device_class=DEVICE_CLASS_TEMPERATURE,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_FAN_SPEED): sensor.sensor_schema(
+                unit_of_measurement=UNIT_REVOLUTIONS_PER_MINUTE,
+                accuracy_decimals=0,
+                device_class=DEVICE_CLASS_SPEED,
+                state_class=STATE_CLASS_MEASUREMENT,
+                icon=ICON_FAN,
+            ),
+            cv.Optional(CONF_SETPOINT): sensor.sensor_schema(
+                unit_of_measurement=UNIT_CELSIUS,
+                accuracy_decimals=1,
+                device_class=DEVICE_CLASS_TEMPERATURE,
+                state_class=STATE_CLASS_MEASUREMENT,
+            )
         }
     )
     .extend(cv.polling_component_schema("60s"))
@@ -29,3 +54,20 @@ async def to_code(config):
     await cg.register_component(var, config)
     await climate.register_climate(var, config)
     await modbus.register_modbus_device(var, config)
+
+    if CONF_AIR_TEMPERATURE in config:
+        conf = config[CONF_AIR_TEMPERATURE]
+        sens = await sensor.new_sensor(conf)
+        cg.add(var.set_air_temperature_sensor(sens))
+    if CONF_WATER_TEMPERATURE in config:
+        conf = config[CONF_WATER_TEMPERATURE]
+        sens = await sensor.new_sensor(conf)
+        cg.add(var.set_water_temperature_sensor(sens))
+    if CONF_FAN_SPEED in config:
+        conf = config[CONF_FAN_SPEED]
+        sens = await sensor.new_sensor(conf)
+        cg.add(var.set_fan_speed_sensor(sens))
+    if CONF_SETPOINT in config:
+        conf = config[CONF_SETPOINT]
+        sens = await sensor.new_sensor(conf)
+        cg.add(var.set_setpoint_sensor(sens))
