eb8103b28eeebe4d016a3c04bca01bde01155b99
pico1881 <82042085+pico1881@users.noreply.github.com>
2025-02-07 18:20:12 +0100

    Update innova_climate.cpp

--- components/innova_climate/innova_climate.cpp
+++ components/innova_climate/innova_climate.cpp
@@ -224,6 +224,13 @@ void Innova::dump_config() {
     LOG_CLIMATE("", "Innova Climate", this); 
     ESP_LOGCONFIG(TAG, "INNOVA:");
     ESP_LOGCONFIG(TAG, "  Address: 0x%02X", this->address_);
+    LOG_SENSOR("", "Air Temperature", this->air_temperature_sensor_);
+    LOG_SENSOR("", "Water Temperature", this->water_temperature_sensor_);
+    LOG_SENSOR("", "FAN speed", this->fan_speed_sensor_);
+    LOG_SENSOR("", "Setpoint", this->setpoint_sensor_);  
+    LOG_BINARY_SENSOR("", "Boiler relay", this->boiler_relay_sensor_);  
+    LOG_BINARY_SENSOR("", "Chiller relay", this->chiller_relay_sensor_); 
+    LOG_SWITCH("", "Innova Switch", this->key_lock_switch);
 }
 
 }  // namespace innova
