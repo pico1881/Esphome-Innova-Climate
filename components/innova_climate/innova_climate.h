75fd1a1554461ab83373054a575acc568300d63f
pico1881 <82042085+pico1881@users.noreply.github.com>
2025-02-07 15:34:36 +0100

    Update innova_climate.h

--- components/innova_climate/innova_climate.h
+++ components/innova_climate/innova_climate.h
@@ -4,6 +4,7 @@
 #include "esphome/components/climate/climate.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/binary_sensor/binary_sensor.h"
+#include "esphome/components/switch/switch.h"
 #include "esphome/core/helpers.h"
 #include <deque>
 
