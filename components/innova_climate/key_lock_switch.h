34373215e4885099d335768be52eba4db472b45e
pico1881 <82042085+pico1881@users.noreply.github.com>
2025-02-08 10:21:11 +0100

    Update key_lock_switch.h

--- components/innova_climate/key_lock_switch.h
+++ components/innova_climate/key_lock_switch.h
@@ -7,12 +7,10 @@ namespace esphome {
 namespace innova {
 
 class InnovaSwitch : public switch_::Switch, public Parented<Innova> {
-
 protected:
   void write_state(bool state) override {
       parent_->set_key_lock(state);
   };
-
 };
 
 }  // namespace innova
